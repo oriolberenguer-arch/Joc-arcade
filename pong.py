db4165f06f1d158cb6f82daba79b51aee37d00b7
oriolberenguer-arch <oriolberenguer@insmiquelbosch.cat>
2025-08-04 18:10:30 +0200

    Update pong.py

--- pong.py
+++ pong.py
@@ -34,9 +34,10 @@ bola.goto(0, 0)
 
 # Funció per moure les pales
 def pala1_amunt():
-  y = pala1.ycor()
-  y += 20
-  pala1.sety(y)
+  if y <= 300:
+    y = pala1.ycor()
+    y += 20
+    pala1.sety(y)
 
 wn.listen()
 wn.onkeypress(pala1_amunt, "w")
