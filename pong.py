cbb8af3dc21174a026f66811b806f765f8744a7b
oriolberenguer-arch <oriolberenguer@insmiquelbosch.cat>
2025-08-04 17:54:34 +0200

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,13 +1,21 @@
-# Importem la llibreria 
 import turtle
 import os
 
+# Configuració de la pantalla
 wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
-wn._root.attributes('-fullscreen', True)
+wn.setup(width=800, height=600) # línia provisional
+# codi real--> wn._root.attributes('-fullscreen', True)
 
-
+# Configuració pales
+pala1 = turtle.Turtle()
+pala1.speed(0)
+pala1.shape("square")
+pala1.color("white")
+pala1.shapesize(stretch_wid=5, stretch_len=1)
+pala1.penup()
+pala1.goto(-350, 0)
 
 while True:
   wn.update()
