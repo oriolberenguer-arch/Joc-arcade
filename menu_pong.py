11c560d81c61a71eeac439f902b058b47cc28a60
oriolberenguer-arch <oriolberenguer@insmiquelbosch.cat>
2025-08-28 18:33:44 +0200

    Update menu_pong.py

--- menu_pong.py
+++ menu_pong.py
@@ -1,16 +1,59 @@
 import turtle
+import tkinter as tk
 import os
+import pong
+import subprocess
 
-# Configuració de la pantalla
-wn = turtle.Screen()
-wn.title("Menu")
-wn.bgcolor("black")
-wn.setup(width=800, height=600) # línia provisional
-# codi real--> wn._root.attributes('-fullscreen', True)
+def crear_menu():
+
+    # Configuració de la pantalla
+    root = tk.Tk()
+    root.title("Menu Pong")
+    root.geometry("800x600")
+    root.configure(bg="black") 
+    # codi real--> wn._root.attributes('-fullscreen', True)
 
 
-while True:
-    wn.update()
+    titol = tk.Label(root, text="PONG", font=("Courier", 36, "bold"),fg="white", bg="#1a1a1a")
+    titol.pack(pady=30)
+
+    # Funcions per cada partida
+    def jugar_partida_curta():
+        root.destroy()
+        root.after(100, lambda: pong.jugar_pong(3))
+
+    def jugar_partida_mitjana():
+        root.destroy()
+        root.after(100, lambda: pong.jugar_pong(3))
+
+    def jugar_partida_llarga():
+        root.destroy()
+        root.after(100, lambda: pong.jugar_pong(3))
+
+    btn_font = ("Courier", 16, "bold")
+    btn_bg = "white"
+    btn_fg = "black"
+    btn_active_bg = "grey"
 
 
+    # Crear botons
+    # Botons de selecció de partida
+    tk.Button(root, text="Partida curta (3 punts)", font=btn_font,
+              bg=btn_bg, fg=btn_fg, activebackground=btn_active_bg,
+              width=25, height=2, command=jugar_partida_curta).pack(pady=10)
+
+    tk.Button(root, text="Partida mitjana (6 punts)", font=btn_font,
+              bg=btn_bg, fg=btn_fg, activebackground=btn_active_bg,
+              width=25, height=2, command=jugar_partida_mitjana).pack(pady=10)
+
+    tk.Button(root, text="Partida llarga (9 punts)", font=btn_font,
+              bg=btn_bg, fg=btn_fg, activebackground=btn_active_bg,
+              width=25, height=2, command=jugar_partida_llarga).pack(pady=10)
+
+
+
+    # Això ha d'estar al final
+    root.mainloop()
+
+crear_menu()
 
