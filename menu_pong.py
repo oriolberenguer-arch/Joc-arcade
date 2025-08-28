222c6e4c9f47897e1df7d3c12ffe4c1e2b0f5469
oriolberenguer-arch <oriolberenguer@insmiquelbosch.cat>
2025-08-28 16:51:20 +0200

    Update menu_pong.py

--- menu_pong.py
+++ menu_pong.py
@@ -1,1 +1,16 @@
+import turtle
+import os
+
+# Configuració de la pantalla
+wn = turtle.Screen()
+wn.title("Menu")
+wn.bgcolor("black")
+wn.setup(width=800, height=600) # línia provisional
+# codi real--> wn._root.attributes('-fullscreen', True)
+
+
+while True:
+    wn.update()
+
+
 
