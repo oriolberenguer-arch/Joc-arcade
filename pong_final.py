f6626d0868150e4fcc85f6d28d5a957472ea8bb1
oriolberenguer-arch <oriolberenguer@insmiquelbosch.cat>
2025-10-26 11:53:53 +0100

    Update pong_final.py

--- pong_final.py
+++ pong_final.py
@@ -1,38 +1,52 @@
-import tkinter as tk
-import pong
-import menu_pong
 import pygame
 
 pygame.init()
 
 
 def pantalla_final(guanyador, puntuacio_max):
-    root = tk.Tk()
-    root.title("Jugador guanyador")
-    root.attributes('-fullscreen', True)
-    root.configure(bg="black") 
+    pygame.init()
+    pygame.joystick.init()
+    pantalla = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Jugador guanyador")
 
 
     def reiniciar():
-        root.destroy()
+        import pong
+        pygame.display.quit()
         pong.jugar_pong(puntuacio_max)
 
     def tornar_menu():
-        root.destroy()
+        import menu_pong     
         menu_pong.crear_menu()
 
 
-    cartell = tk.Label(root, text=f"JUGADOR {guanyador} GUANYADOR", font=("Courier", 72, "normal"), fg="white", bg="black")
-    cartell.pack(pady=75)
+    font_titol = pygame.font.SysFont("Courier", 100, bold=True)
+    titol_surface = font_titol.render(f"JUGADOR {guanyador} GUANYADOR", True, (255, 255, 255))
+    titol_rect = titol_surface.get_rect(center=(pantalla.get_width() // 2, 100))
+    
 
 
-    boto_reiniciar = tk.Button(root, text="Reiniciar", font=("Courier", 36, "normal"), fg="black", bg="white", activebackground="grey",
-                               command=reiniciar)
-    boto_menu = tk.Button(root, text="Tornar al menú", font=("Courier", 36, "normal"), fg="black", bg="white", activebackground="grey",
-                          command=tornar_menu)
-    boto_reiniciar.pack()
-    boto_menu.pack()
+    boto_reiniciar = {"rect": pygame.Rect(380, 220, 600, 140), "text": "Reiniciar"}
+    boto_menu = {"rect": pygame.Rect(380, 390, 600, 140), "text": "Tornar al menú"}
+    running = True
+    while running:
+        pantalla.fill((0, 0, 0))
+        pantalla.blit(titol_surface, titol_rect)
+        for boto in [boto_reiniciar, boto_menu]:
+            pygame.draw.rect(pantalla, "white", boto["rect"])
+            text = pygame.font.SysFont("Courier", 28, bold=True).render(boto["text"], True, "black")
+            pantalla.blit(text, text.get_rect(center=boto["rect"].center))
 
 
-    root.mainloop()
-
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.JOYBUTTONDOWN:
+                    if event.button == 1 or event.button == 3:
+                        reiniciar()
+                        running = False
+                    elif event.button == 0 or event.button == 2:
+                        print("tornaar al menu")
+                        running = False
+                        tornar_menu()
+        pygame.display.flip()
